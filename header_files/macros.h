365c05413fe1e0135dde919f374994a3b8b60454
Ana Ternero Mayorga <166392499+anatermay@users.noreply.github.com>
2025-08-28 09:36:20 +0000

    Paso de pensar en inglés Escribo esto en español: he creado dos header_file para mayor claridad del principal, aunque después se fusionen todos. También están los archivos numerados por orden de ejecución y creación y los que están con el prefijo u_ hacen referencia a útiles. Están incluidos mi libft y mi ft_printf. También he creado el Makefile, a falta de incluir la MLX42, y dos mapas de ejemplo para hacer las primeras comprobaciones. Hasta el momento, el código debería funcionar hasta abrir el archivo y leer su contenido. Haré la comprobación tras este commit antes de comenzar a procesar todo el contenido del archivo .cub. No sé si se me olvida comentar algo. El .gitignore es para evitar que se suva el vscode y archivos extraños.

---
+++ header_files/macros.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   macros.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aternero <aternero@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/27 12:32:25 by aternero          #+#    #+#             */
+/*   Updated: 2025/08/28 11:13:33 by aternero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MACROS_H
+# define MACROS_H
+
+// BASIC BOOLS
+# define ERROR -1
+# define FALSE 0
+# define TRUE 1
+
+//	COORDINATES
+# define NORTH "NO"
+# define NAC 'N'
+# define SOUTH "SO"
+# define SAC 'S'
+# define EAST "EA"
+# define EAC 'E'
+# define WEST "WE"
+# define WAC 'W'
+# define FLOOR 'F'
+# define CEILING 'C'
+
+#endif
--- utils/p_print.c
+++
@@ -1,8 +0,0 @@
-
-#include "../cub3d.h"
-
-int p_print(char *msg)
-{
-	ft_printf("Error\n\t%s", msg);
-	return (FALSE);
-}
