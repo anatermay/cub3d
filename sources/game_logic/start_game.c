f6730ef205970675e681fef4c327bd289d5ec133
JordiDevv <jordidevv@gmail.com>
2025-10-11 14:33:18 +0200

    Se ha adaptado el start_game para que trabaje ya con la estructura del parseo tras hacer algunas comprobaciones. Se han borrado los ficheros del testeo y estaría bien tener la dirección en la que mira el jugador almacenada para que sea más fácil acceder a ella. Seguimos trabajando en 2D para escribir la lógica base. De momento hace el display de pantalla correctamente.

--- sources/game_logic/start_game.c
+++ sources/game_logic/start_game.c
@@ -6,37 +6,40 @@
 /*   By: jsanz-bo <jsanz-bo@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 12:29:12 by jsanz-bo          #+#    #+#             */
-/*   Updated: 2025/09/27 12:22:59 by jsanz-bo         ###   ########.fr       */
+/*   Updated: 2025/10/11 14:25:07 by jsanz-bo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-#include "testing.h"
 
-static void	get_images(t_game *game, t_testing_img *assets)
+static void	get_images(t_game *game)
 {
-	assets->wall_tex = mlx_load_png("textures/wall_tex.png");
-	assets->floor_tex = mlx_load_png("textures/floor_tex.png");
-	assets->player_tex = mlx_load_png("textures/player_tex.png");
-	assets->wall_img = mlx_texture_to_image(game->mlx, assets->wall_tex);
-	assets->floor_img = mlx_texture_to_image(game->mlx, assets->floor_tex);
-	assets->player_img = mlx_texture_to_image(game->mlx, assets->player_tex);
+	game->north->tex->text = mlx_load_png("imgs/wall_01.png");
+	game->south->tex->text = mlx_load_png("imgs/wooden_wall_chopper.png");
+	game->west->tex->text = mlx_load_png("imgs/malaca_01.png");
+	game->east->tex->text = mlx_load_png("imgs/wooden-wall_door.png");
+	game->north->tex->img = mlx_texture_to_image(game->mlx,
+		game->north->tex->text);
+	game->south->tex->img = mlx_texture_to_image(game->mlx,
+		game->south->tex->text);
+	game->west->tex->img = mlx_texture_to_image(game->mlx,
+		game->west->tex->text);
+	game->east->tex->img = mlx_texture_to_image(game->mlx,
+		game->east->tex->text);
 }
 
-static void	display_image(t_game *game, t_testing_img *assets, int i, int j)
+static void	display_image(t_game *game, int i, int j)
 {
 	char	c;
 
 	c = game->map->map[i][j];
 	if (c == '1')
-		mlx_image_to_window(game->mlx,
-			assets->wall_img, j * 64, i * 64);
+		mlx_image_to_window(game->mlx, game->north->tex->img, j * 64, i * 64);
 	if (c == 'N' || c == 'E' || c == 'W' || c == 'S')
-		mlx_image_to_window(game->mlx,
-			assets->player_img, j * 64, i * 64);
+		mlx_image_to_window(game->mlx, game->south->tex->img, j * 64, i * 64);
 }
 
-static void	display_map(t_game *game, t_testing_img *assets)
+static void	display_map(t_game *game)
 {
 	int	i;
 	int	j;
@@ -47,8 +50,8 @@ static void	display_map(t_game *game, t_testing_img *assets)
 		j = 0;
 		while (j < game->map->dim.x)
 		{
-			mlx_image_to_window(game->mlx, assets->floor_img, j * 64, i * 64);
-			display_image(game, assets, i, j);
+			mlx_image_to_window(game->mlx, game->west->tex->img, j * 64, i * 64);
+			display_image(game, i, j);
 			j++;
 		}
 		i++;
@@ -57,15 +60,12 @@ static void	display_map(t_game *game, t_testing_img *assets)
 
 void	start_game(t_game *game)
 {
-	t_testing_img	*assets;
-
-	assets = malloc(sizeof(t_testing_img));
 	game->mlx = mlx_init(game->map->dim.x * 64, game->map->dim.y * 64,
                 "cub3D", true);
-	get_images(game, assets);
-	display_map(game, assets);
+	get_images(game);
+	display_map(game);
 	// mlx_key_hook(game->mlx, key_controller, data_game);
-	// mlx_loop(game->mlx);
+	mlx_loop(game->mlx);
 	// if (game->collected == game->collects_total && game->exit)
 	// 	ft_printf("You won!\n");
 	// free_images(game);
