1d59b73430f9d7c9aceb2bdd4605dda5848356f1
Ana Ternero Mayorga <anaternero98@gmail.com>
2025-11-13 20:44:34 +0100

    Hemos realizado últimas correcciones: relativas a la verificaciones de las paredes con floodfill, donde se daban casos de segmentation fault; relativas al retorno en casos donde el número RGB excedía de algún modo los límites, por un fallo a la hora de contemplar el valor de retorno de la función encargada de checkearlo. También hemos modificado el map2.cub por mera estética.

--- sources/parser/16_flood_fill.c
+++ sources/parser/16_flood_fill.c
@@ -6,50 +6,37 @@
 /*   By: aternero <aternero@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 18:06:26 by aternero          #+#    #+#             */
-/*   Updated: 2025/11/13 18:08:48 by aternero         ###   ########.fr       */
+/*   Updated: 2025/11/13 20:15:21 by aternero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../header_files/cub3d.h"
 
-static int	valid_surroundings(char c)
+/* static int	valid_surroundings(char c)
 {
-	if (c == '1')
+	if (c == '1' || c == '0' || is_position(c) == TRUE)
 		return (TRUE);
-	else if (is_space(c) == TRUE)
-		return (print_error(EWALLS));
-	else if (c == '0' || is_position(c))
-		return (TRUE);
-	else if (c != '0' && c != '1' && is_space(c) == FALSE
-		&& is_position(c) == FALSE)
-		return (print_error(EWALLS));
 	return (print_error(EWALLS));
-}
+} */
 
 static int	fill_check(char **map, t_dim dim, t_dim init)
 {
-	if (map)
+	if (!map)
+		return (TRUE);
+	if (init.x < 0 || init.y < 0 || init.y >= dim.y
+		|| init.x >= (int)ft_strlen(map[init.y])
+		|| is_space(map[init.y][init.x]) == TRUE)
+		return (print_error(EWALLS));
+	if (map[init.y][init.x] == '1' || map[init.y][init.x] == 'D')
+		return (TRUE);
+	if (map[init.y][init.x] == '0' || is_position(map[init.y][init.x]) == TRUE)
 	{
-		if (init.x < 0 || init.y < 0 || init.y >= dim.y
-			|| (int)ft_strlen(map[init.y]) <= init.x
-			|| is_space(map[init.y][init.x]) == TRUE
-			|| map[init.y][init.x] == '1')
-			return (TRUE);
-		if (map[init.y][init.x] == '0'
-			|| is_position(map[init.y][init.x]) == TRUE)
-		{
-			if (valid_surroundings(map[dim.y + 1][dim.x]) == FALSE
-			|| valid_surroundings(map[dim.y - 1][dim.x]) == FALSE
-			|| valid_surroundings(map[dim.y][dim.x + 1]) == FALSE
-			|| valid_surroundings(map[dim.y][dim.x - 1]) == FALSE)
-				return (print_error("line 60: HI"));
-			map[init.y][init.x] = 'D';
-		}
+		map[init.y][init.x] = 'D';
 		if (fill_check(map, dim, (t_dim){init.x - 1, init.y}) == FALSE
 			|| fill_check(map, dim, (t_dim){init.x + 1, init.y}) == FALSE
 			|| fill_check(map, dim, (t_dim){init.x, init.y - 1}) == FALSE
 			|| fill_check(map, dim, (t_dim){init.x, init.y + 1}) == FALSE)
-			return (print_error("line 73: HI"));
+			return (FALSE);
 	}
 	return (TRUE);
 }
