42e88fad31888f8a99646c243dc41b6f134adda0
Ana Ternero Mayorga <anaternero98@gmail.com>
2025-11-23 15:57:48 +0100

    he solucionado varios errores de memoria, pero queda por revisar y aclarar.

--- helpsh/current-code.sh
+++
@@ -1,6 +0,0 @@
-
-cat Makefile > current-code.md
-cat header_files/*.h >> current-code.md
-cat sources/*.c >> current-code.md
-# cat sources/main.c >> current-code.md
-# cat sources/[0-9]*.c | grep -v '^pu_' >> current-code.md
